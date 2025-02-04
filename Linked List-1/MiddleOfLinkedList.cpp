bbed7c71e1d53f092deca98954a3a43dee10fc7e
Ramesh Das <rameshdas@Rameshs-MacBook-Air.local>
2025-02-04 11:58:46 +0530

    Leetcode 876 Middle of a Linked List

---
+++ Linked List-1/MiddleOfLinkedList.cpp
@@ -0,0 +1,69 @@
+#include <iostream>
+
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode* next;
+    ListNode(int x) : val(x), next(NULL) {}
+};
+
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        ListNode* prev = NULL;
+        ListNode* temp = head;
+        while (temp != NULL) {
+            ListNode* front = temp->next;
+            temp->next = prev;
+            prev = temp;
+            temp = front;
+        }
+        return prev;
+    }
+
+    ListNode* middleNode(ListNode* head) {
+        ListNode* fast = head;
+        ListNode* slow = head;
+        while (fast != NULL && fast->next != NULL) {
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+        return slow;
+    }
+};
+
+// Function to print the linked list
+void printList(ListNode* head) {
+    while (head != NULL) {
+        cout << head->val << " -> ";
+        head = head->next;
+    }
+    cout << "NULL" << endl;
+}
+
+int main() {
+    // Creating a sample linked list: 1 -> 2 -> 3 -> 4 -> 5 -> NULL
+    ListNode* head = new ListNode(1);
+    head->next = new ListNode(2);
+    head->next->next = new ListNode(3);
+    head->next->next->next = new ListNode(4);
+    head->next->next->next->next = new ListNode(5);
+
+    cout << "Original List: ";
+    printList(head);
+
+    // Reverse the linked list
+    Solution sol;
+    head = sol.reverseList(head);
+
+    cout << "Reversed List: ";
+    printList(head);
+
+    // Find middle node
+    head = sol.reverseList(head); // Reverse again to get the original order
+    ListNode* middle = sol.middleNode(head);
+    cout << "Middle Node: " << middle->val << endl;
+
+    return 0;
+}
